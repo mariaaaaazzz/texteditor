d22c75a860b930541a2bc4432347413b656ec160
zhangjiayi <zhangjiayi@zhangjiayis-MacBook-Pro.local>
2025-10-06 20:54:33 -0500

    first draft done

--- src/main/java/edu/grinnell/csc207/texteditor/GapBuffer.java
+++ src/main/java/edu/grinnell/csc207/texteditor/GapBuffer.java
@@ -5,23 +5,36 @@ import java.util.Arrays;
  * A gap buffer-based implementation of a text buffer.
  */
 public class GapBuffer {
-    private int cursor;//gapStart
+    private int cursor; //gapStart
     private int gapEnd;
     private char[] data;
-    //private int size;//data.length - (gapEnd-cursor)
 
 
+    /**
+     * Creates an empty gap buffer with an initial capacity of 10 characters.
+     */
     public GapBuffer(){
         data = new char[10];
         cursor = 0;
         gapEnd = data.length;
     }
+
+    /**
+     * Inserts a character at the current cursor position.
+     * The cursor then moves one position to the right.
+     *
+     * @param ch the character to insert
+     */
     public void insert(char ch) {
         ensureGap(1);
         data[cursor]=ch;
         cursor++;
     }
 
+    /**
+     * Deletes the character to the left of the cursor, if there is one.
+     * The cursor then moves one position to the left.
+     */
     public void delete() {
         if (cursor == 0){
             return;
@@ -29,10 +42,18 @@ public class GapBuffer {
         cursor--;
     }
 
+    /**
+     * Returns the current cursor position.
+     *
+     * @return the index where the next character will be inserted
+     */
     public int getCursorPosition() {
         return cursor;
     }
 
+    /**
+     * Moves the cursor one position to the left.
+     */
     public void moveLeft() {
         if (cursor > 0){
             data[cursor-1]=data[gapEnd-1];
@@ -41,6 +62,9 @@ public class GapBuffer {
         }
     }
 
+    /**
+     * Moves the cursor one position to the right.
+     */
     public void moveRight() {
         if (cursor < data.length){
             data[cursor]=data[gapEnd];
@@ -49,11 +73,23 @@ public class GapBuffer {
         }
     }
 
+    /**
+     * Returns the number of characters currently stored in the buffer.
+     *
+     * @return the number of characters in the buffer.
+     */
     public int getSize() {
         int size = data.length - (gapEnd-cursor);
         return size;
     }
 
+    /**
+     * Returns the character at the given index in the buffer.
+     *
+     * @param i the index of the character to retrieve
+     * @return the character at the specified index
+     * @throws IndexOutOfBoundsException if the index is invalid
+     */
     public char getChar(int i) {
         if (i<0 || i > getSize()){
             throw new IndexOutOfBoundsException();
@@ -66,6 +102,12 @@ public class GapBuffer {
   
     }
 
+
+    /**
+     * Returns the contents of the buffer as a String.
+     *
+     * @return the buffer's contents as a string
+     */
     public String toString() {
         String result = "";
         for (int i = 0; i < cursor; i++) {
@@ -77,6 +119,12 @@ public class GapBuffer {
         return result;
     }
 
+    /**
+     * Ensures that the gap has enough space for the given number of characters.
+     * If not, the array is resized and the right chunk is moved to the end.
+     *
+     * @param gapNeed the minimum gap size required
+     */
     private void ensureGap(int gapNeed){
        int gapSize = gapEnd - cursor;
        if (gapSize >= gapNeed){
--- src/main/java/edu/grinnell/csc207/texteditor/SimpleStringBuffer.java
+++ src/main/java/edu/grinnell/csc207/texteditor/SimpleStringBuffer.java
@@ -11,12 +11,25 @@ public class SimpleStringBuffer {
     private int size; //size of string data
     private char[] data;
 
+    /**
+     * Creates a new text buffer with the given initial capacity.
+     *
+     * @param cursor the initial cursor position (usually 0)
+     * @param size   the initial capacity of the buffer
+     */
     SimpleStringBuffer(int cursor, int size){
         this.cursor = 0;
         this.size = 0;
         data = new char[size];
     }
 
+    /**
+     * Inserts a character at the current cursor position.
+     * All existing characters from the cursor onward are shifted one position to the right.
+     * The cursor then moves one position forward.
+     *
+     * @param ch the character to insert
+     */
     public void insert(char ch) {
         ensureCapacity();
         int i = cursor;
@@ -29,6 +42,12 @@ public class SimpleStringBuffer {
         cursor++;
     }
 
+    /**
+     * Deletes the character to the left of the cursor (backspace behavior).
+     * The remaining characters are shifted left to fill the gap.
+     * The cursor then moves one position backward.
+     * If the cursor is at position 0, nothing happens.
+     */
     public void delete() {
         ensureCapacity();
         int i = cursor;
@@ -40,11 +59,20 @@ public class SimpleStringBuffer {
         cursor--;
     }
 
+    /**
+     * Returns the current cursor position in the buffer.
+     *
+     * @return the index where the next character will be inserted
+     */
     public int getCursorPosition() {
         ensureCapacity();
         return cursor;
     }
 
+    /**
+     * Moves the cursor one position to the left, if possible.
+     * If the cursor is already at the beginning, it does nothing.
+     */
     public void moveLeft() {
         ensureCapacity();
         if (cursor != 0){
@@ -52,6 +80,10 @@ public class SimpleStringBuffer {
         }
     }
 
+    /**
+     * Moves the cursor one position to the right, if possible.
+     * If the cursor is already at the end of the buffer, it does nothing.
+     */
     public void moveRight() {
         ensureCapacity();
         if (cursor != size){
@@ -59,11 +91,24 @@ public class SimpleStringBuffer {
         }
     }
 
+
+    /**
+     * Returns the number of characters currently stored in the buffer.
+     *
+     * @return the size of the buffer
+     */
     public int getSize() {
         ensureCapacity();
         return size;
     }
 
+    /**
+     * Returns the character at the specified index.
+     *
+     * @param i the index of the character to retrieve
+     * @return the character at the given position
+     * @throws IndexOutOfBoundsException if the index is invalid
+     */
     public char getChar(int i) {
         ensureCapacity();
         if (i<0 || i>=size){
@@ -72,13 +117,21 @@ public class SimpleStringBuffer {
         return data[i];
     }
 
+    /**
+     * Ensures that the buffer has enough capacity to hold new characters.
+     * If the buffer is full, its size is doubled.
+     */
     private void ensureCapacity(){
         if(size==data.length){
             data = Arrays.copyOf(data, data.length * 2);
         }
     }
 
-    @Override
+    /**
+     * Returns the contents of the buffer as a String.
+     *
+     * @return the text stored in the buffer
+     */
     public String toString() {
         ensureCapacity();
         int i = 0;
